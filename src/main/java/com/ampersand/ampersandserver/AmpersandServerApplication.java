809baa325e8546c40b192d57d8742165b547eaf6
Chunghwan Kim <49121847+ch0213@users.noreply.github.com>
2023-04-11 01:34:23 +0900

    프로젝트 생성 (#2)

---
+++ src/main/java/com/ampersand/ampersandserver/AmpersandServerApplication.java
@@ -0,0 +1,13 @@
+package com.ampersand.ampersandserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpersandServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpersandServerApplication.class, args);
+	}
+
+}
