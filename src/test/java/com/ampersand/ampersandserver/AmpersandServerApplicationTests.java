e322d59c5fc0542209662566c20fafd319d6fcb2
ch0213 <cndghks0213@gmail.com>
2023-04-11 01:29:52 +0900

    프로젝트 생성

---
+++ src/test/java/com/ampersand/ampersandserver/AmpersandServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ampersand.ampersandserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpersandServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
