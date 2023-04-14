fc147354ce5776273f13f8b58a6b18c07a5f5deb
ch0213 <cndghks0213@gmail.com>
2023-04-14 16:13:17 +0900

    lint 적용

--- ampersand-api/src/main/java/com/ampersand/AmpersandServerApplication.java
+++ ampersand-api/src/main/java/com/ampersand/AmpersandServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AmpersandServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmpersandServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AmpersandServerApplication.class, args);
+    }
 }
