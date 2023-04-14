2360173bde1279e39bb3aa38c79668642a27e293
ch0213 <cndghks0213@gmail.com>
2023-04-14 16:11:29 +0900

    프로젝트 초기 설정

--- ampersand-api/src/main/java/com/ampersand/AmpersandServerApplication.java
+++ ampersand-api/src/main/java/com/ampersand/AmpersandServerApplication.java
@@ -1,4 +1,4 @@
-package com.ampersand.ampersandserver;
+package com.ampersand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ampersand-api/src/test/java/com/ampersand/AmpersandServerApplicationTests.java
+++ ampersand-api/src/test/java/com/ampersand/AmpersandServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ampersand.ampersandserver;
+package com.ampersand;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
