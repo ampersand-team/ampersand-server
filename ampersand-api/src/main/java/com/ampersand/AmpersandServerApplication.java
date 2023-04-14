5b21e531b941c35dfb2dc3e16c97dc44b11faf9f
Chunghwan Kim <49121847+ch0213@users.noreply.github.com>
2023-04-14 16:24:06 +0900

    프로젝트 초기 설정 (#5)
    * 프로젝트 초기 설정
    
    * lint 적용

--- ampersand-api/src/main/java/com/ampersand/AmpersandServerApplication.java
+++ ampersand-api/src/main/java/com/ampersand/AmpersandServerApplication.java
@@ -1,4 +1,4 @@
-package com.ampersand.ampersandserver;
+package com.ampersand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AmpersandServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AmpersandServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AmpersandServerApplication.class, args);
+    }
 }
