fb80b3913d6aa9930bd5a708419df14df3602ac8
ch0213 <cndghks0213@gmail.com>
2023-05-09 13:08:35 +0900

    refactor: MemberNotFoundException에 ErrorCode 추가

--- ampersand-domain/src/main/java/com/ampersand/domain/member/exception/MemberNotFoundException.java
+++ ampersand-domain/src/main/java/com/ampersand/domain/member/exception/MemberNotFoundException.java
@@ -1,9 +1,10 @@
 package com.ampersand.domain.member.exception;
 
 import com.ampersand.common.exception.NotFoundException;
+import com.ampersand.domain.DomainErrorCode;
 
 public class MemberNotFoundException extends NotFoundException {
     public MemberNotFoundException(String message) {
-        super(message);
+        super(DomainErrorCode.MEMBER_NOT_FOUND, message);
     }
 }
